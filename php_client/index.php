9cdcf1816798d574f6019d2f31845c7cb1a630a2
Patrícia Olšavská <patricia.olsavska@student.tuke.sk>
2020-11-19 16:49:27 +0100

    add ziveit

--- php_client/index.php
+++ php_client/index.php
@@ -12,26 +12,71 @@ $clientBuilder = ClientBuilder::create();   // Vytvorenie noveho ClientBuilder-a
 $clientBuilder->setHosts($hosts);           // Nastavenie hostu
 $client = $clientBuilder->build();
 
-$name = 'Laser';
+$name = 'Laser';  // nazov
+$value = '411';   // produktovy kod
+$value_id = '180246'; // id
+
+//$params = [
+//  'index' => 'produkty',
+//  'body'  => [
+//    'query' => [
+//      'bool' => [
+//        'should' => [
+//          ['wildcard' => ['Nazov' => '*' . $name . '*'] ]
+//        ]
+//      ]
+//    ]
+//  ]
+//];
 
 $params = [
-  'index' => 'produkty',
-  'body'  => [
-    'query' => [
-      'bool' => [
-        'should' => [
-          ['wildcard' => ['Nazov' => '*' . $name . '*']]
+    'index' => 'produkty',
+    'body' => [
+        'query' => [
+            "bool" => [
+                'should' => [
+                    [
+                        'match' => [
+                            'Id' => [
+                                'query' => $value_id,
+                            ]
+                        ]
+                    ],
+                    [
+                        'match' => [
+                            'Nazov' => [
+                                'query' => $name,
+                            ]
+                        ]
+                    ],
+                    [
+                        'match' => [
+                            'Produktovy kod' => [
+                                'query' => $value,
+                            ]
+                        ]
+                    ]
+
+                    /* [
+                        'multi_match' => [
+                            'query' => $query,
+                            'fuzziness' => 'AUTO',
+                            "prefix_length" => 0,
+                            "max_expansions" => 100,
+                            'fields' => ['product_number', 'product_name', 'producer_name'],
+                        ]
+                    ] */
+                ]
+            ]
         ]
-      ]
     ]
-  ]
 ];
 
 $results = $client->search($params);
 
-// print_r($results);
+//print_r($results);
 
-// echo "</br> </br>";
+//echo '</pre>', $results, ' </pre>';
 
 foreach ($results['hits']['hits'] as $result => $value) {
   print_r($value['_source']);
