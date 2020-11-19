f088877255f25ef7edbf3329540a13979b198bb3
Igor Sivy <igorsivy@gmail.com>
2020-11-19 17:16:10 +0100

    Add basic implementation for search

--- ziveit/api/src/controllers/index_ctrl.ts
+++ ziveit/api/src/controllers/index_ctrl.ts
@@ -1,28 +1,33 @@
 import {resJson} from "../helpers/utils";
 
-export const test = async (ctx): Promise<void> => {
-  resJson(ctx, {});
-};
-
 export const openapiJson = async (ctx): Promise<void> => {
   ctx.koa.response.set('X-Auth-Type', 'application/json');
   resJson(ctx, ctx.api.openApiDoc);
 };
 
-export const test2 = async (ctx): Promise<void> => {
-
-  // const config = coig.get('elasticsearch');
-  // const service = new ElasticSearch(config);
-  // const data = await service.get('query:{products: wildcarcd}')
-
-  resJson(ctx, {test: 1233});
-};
-
 export const search = async (ctx): Promise<void> => {
+  const config = require('config');
+  const elasticsearchConfig = config.get('elasticsearch');
 
-  // const config = coig.get('elasticsearch');
-  // const service = new ElasticSearch(config);
-  // const data = await service.get('query:{products: wildcarcd}')
+  const { Client } = require('@elastic/elasticsearch');
+  const client = new Client(elasticsearchConfig);
 
-  resJson(ctx, ctx.values());
+  const result = await client.search({
+    index: 'produkty_index',
+    body: {
+      query: {
+        bool: {
+          should: {
+              wildcard: {
+                'Nazov': {
+                  value: '*laser*'
+                }
+              }
+          }
+        }
+      }
+    }
+  });
+
+  resJson(ctx, result);
 };
