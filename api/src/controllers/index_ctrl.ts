aa11d55f644a33b63465d2d641f194487b945556
Igor Sivy <igorsivy@gmail.com>
2020-12-21 01:53:15 +0100

    Parse xml and create an index from it.

--- api/src/controllers/index_ctrl.ts
+++ api/src/controllers/index_ctrl.ts
@@ -13,8 +13,52 @@ export const search = async (ctx): Promise<void> => {
   const { Client } = require('@elastic/elasticsearch');
   const client = new Client(elasticsearchConfig);
 
+  const configCatalogExists = await client.indices.exists({
+    index: 'config-katalog'
+  });
+
+  if (!configCatalogExists['body']) {
+  const fs = require('fs');
+    const fileContents = fs.readFileSync('config/config-katalog.xml');
+    const xml2js = require('xml2js');
+  
+    var parser = new xml2js.Parser({explicitArray: false});
+    parser.parseString(fileContents, (err, result) => {
+      // Create a dataset of items in the config file
+      const dataset = []
+      const menuItems = result['config']['default']['application-menu']['application-menu-item'];
+
+      for (const menuItem of menuItems) {
+        dataset.push({
+          "Nazov": menuItem['name']
+        })
+
+        for (const submenuItem of menuItem['submenu']['application-menu-item']) {
+          dataset.push({
+            "Nazov": submenuItem['name']
+          })
+        }
+      }
+
+      // Create an index with the dataset
+      client.indices.create({
+        index: 'config-katalog',
+        body: {
+          "mappings": {
+            "properties": {
+              "Nazov": { "type": "text" },
+            }
+          }
+        }
+      });
+
+      const body = dataset.flatMap(doc => [{ index: { _index: "config-katalog"} }, doc]);
+      const { body: bulkResponse } = client.bulk({ refresh: true, body })
+    });
+  }
+
   const result = await client.search({
-    index: 'products',
+    index: ['products', 'config-katalog'],
     body: {
       query: {
         bool: {
@@ -28,15 +72,6 @@ export const search = async (ctx): Promise<void> => {
         }
       }
     }
-  });
-
-  const fs = require('fs');
-  const fileContents = fs.readFileSync('config/config-katalog.xml');
-  const xml2js = require('xml2js');
-
-  var parser = new xml2js.Parser();
-  parser.parseString(fileContents, (err, result) => {
-    // result['config']['default']['0']['application-menu']['0']
   });
 
   resJson(ctx, result);
