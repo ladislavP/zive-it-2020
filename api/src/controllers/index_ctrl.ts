56dd0cf7d1d5e6d97d0de237d07a0dd62c241c60
Ladislav Perduk <perduk@bart.sk>
2020-12-21 15:51:41 +0100

    Small refactoring

--- api/src/controllers/index_ctrl.ts
+++ api/src/controllers/index_ctrl.ts
@@ -7,55 +7,53 @@ export const openapiJson = async (ctx): Promise<void> => {
 };
 
 export const search = async (ctx): Promise<void> => {
-  const config = require('config');
   const query = ctx.params.query.search_query;
 
   const client = ElasticSearchService.loadClient();
 
-  const configCatalogExists = await client.indices.exists({
-    index: 'config-katalog'
-  });
+  // const configCatalogExists = await client.indices.exists({
+  //   index: 'config-katalog'
+  // });
 
-  if (!configCatalogExists['body']) {
-  const fs = require('fs');
-    const fileContents = fs.readFileSync('config/config-katalog.xml');
-    const xml2js = require('xml2js');
+  // if (!configCatalogExists['body']) {
+  // const fs = require('fs');
+  //   const fileContents = fs.readFileSync('config/config-katalog.xml');
+  //   const xml2js = require('xml2js');
   
-    var parser = new xml2js.Parser({explicitArray: false});
-    parser.parseString(fileContents, (err, result) => {
-      // Create a dataset of items in the config file
-      const dataset = []
-      const menuItems = result['config']['default']['application-menu']['application-menu-item'];
+  //   var parser = new xml2js.Parser({explicitArray: false});
+  //   parser.parseString(fileContents, (err, result) => {
+  //     // Create a dataset of items in the config file
+  //     const dataset = []
+  //     const menuItems = result['config']['default']['application-menu']['application-menu-item'];
 
-      for (const menuItem of menuItems) {
-        dataset.push({
-          "Nazov": menuItem['name']
-        })
+  //     for (const menuItem of menuItems) {
+  //       dataset.push({
+  //         "Nazov": menuItem['name']
+  //       })
 
-        for (const submenuItem of menuItem['submenu']['application-menu-item']) {
-          dataset.push({
-            "Nazov": submenuItem['name']
-          })
-        }
-      }
+  //       for (const submenuItem of menuItem['submenu']['application-menu-item']) {
+  //         dataset.push({
+  //           "Nazov": submenuItem['name']
+  //         })
+  //       }
+  //     }
 
-      // Create an index with the dataset
-      client.indices.create({
-        index: 'config-katalog',
-        body: {
-          "mappings": {
-            "properties": {
-              "Nazov": { "type": "text" },
-            }
-          }
-        }
-      });
+  //     // Create an index with the dataset
+  //     client.indices.create({
+  //       index: 'config-katalog',
+  //       body: {
+  //         "mappings": {
+  //           "properties": {
+  //             "Nazov": { "type": "text" },
+  //           }
+  //         }
+  //       }
+  //     });
 
-      const body = dataset.flatMap(doc => [{ index: { _index: "config-katalog"} }, doc]);
-      const { body: bulkResponse } = client.bulk({ refresh: true, body })
-    });
-  }
-
+  //     const body = dataset.flatMap(doc => [{ index: { _index: "config-katalog"} }, doc]);
+  //     const { body: bulkResponse } = client.bulk({ refresh: true, body })
+  //   });
+  // }
   const result = await client.search({
     index: ['products', 'config-katalog'],
     body: {
