847bfa3525e0e5b5a52097e290a872bbb7292a84
Igor Sivy <igorsivy@gmail.com>
2021-01-15 10:14:34 +0100

    Fix toners index lookup.

--- api/src/controllers/index_ctrl.ts
+++ api/src/controllers/index_ctrl.ts
@@ -11,49 +11,6 @@ export const search = async (ctx): Promise<void> => {
 
   const client = ElasticSearchService.loadClient();
 
-  // const configCatalogExists = await client.indices.exists({c
-  //   index: 'config-katalog'
-  // });
-
-  // if (!configCatalogExists['body']) {
-  // const fs = require('fs');
-  //   const fileContents = fs.readFileSync('config/config-katalog.xml');
-  //   const xml2js = require('xml2js');
-  
-  //   var parser = new xml2js.Parser({explicitArray: false});
-  //   parser.parseString(fileContents, (err, result) => {
-  //     // Create a dataset of items in the config file
-  //     const dataset = []
-  //     const menuItems = result['config']['default']['application-menu']['application-menu-item'];
-
-  //     for (const menuItem of menuItems) {
-  //       dataset.push({
-  //         "Nazov": menuItem['name']
-  //       })
-
-  //       for (const submenuItem of menuItem['submenu']['application-menu-item']) {
-  //         dataset.push({
-  //           "Nazov": submenuItem['name']
-  //         })
-  //       }
-  //     }
-
-  //     // Create an index with the dataset
-  //     client.indices.create({
-  //       index: 'config-katalog',
-  //       body: {
-  //         "mappings": {
-  //           "properties": {
-  //             "Nazov": { "type": "text" },
-  //           }
-  //         }
-  //       }
-  //     });
-
-  //     const body = dataset.flatMap(doc => [{ index: { _index: "config-katalog"} }, doc]);
-  //     const { body: bulkResponse } = client.bulk({ refresh: true, body })
-  //   });
-  // }
   const result = await client.search({
     index: ['products', 'config-katalog', 'toners'],
     body: {
@@ -65,14 +22,7 @@ export const search = async (ctx): Promise<void> => {
                 value: query + "*"
               }
             }
-          },
-            {
-              wildcard: {
-                'column5': {
-                  value: query + "*"
-                }
-              }
-            }]
+          }]
         }
       }
     }
